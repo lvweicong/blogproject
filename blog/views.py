63c85c5982aed61f49771d9a2493c1bd26fe03b5
lvweicong <316421657@qq.com>
2017-09-26 15:55:05 +0800

    setting update

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,9 @@ def index(request):
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     # 记得在顶部引入 markdown 模块
+
+    post.increase_views()
+
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                      'markdown.extensions.extra',
